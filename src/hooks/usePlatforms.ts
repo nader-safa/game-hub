75422dfed31575437a95bae51ad087ec0b534bbd
nader-safa <125364885+nader-safa@users.noreply.github.com>
2024-04-12 09:51:56 +0200

    implemented react query on platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,20 @@
-import useData from './useData'
+import { useQuery } from '@tanstack/react-query'
+import { FetchResponse } from './useData'
 import { Platform } from './useGames'
+import apiClient from '../services/api-client'
+import platforms from '../data/platforms'
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () =>
+  useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ['platforms'],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: platforms,
+  })
+
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
 
 export default usePlatforms
