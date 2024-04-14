a209c79c176db3f71e74579bd272f15a09bfa5c9
nader-safa <125364885+nader-safa@users.noreply.github.com>
2024-04-14 07:42:18 +0200

    enhanced game details page layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spinner } from '@chakra-ui/react'
+import { Box, GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import ExpandableText from '../components/expandableText'
 import GameAttributes from '../components/gameAttributes'
@@ -16,15 +16,21 @@ const GameDetailPage = () => {
   if (error || !game) throw error
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <Box my={3}>
-        <ExpandableText>{game.description_raw}</ExpandableText>
-      </Box>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <Box my={3}>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+        </Box>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <Box mt={2}>
+          <GameScreenshots gameId={game.id} />
+        </Box>
+      </GridItem>
+    </SimpleGrid>
   )
 }
 
